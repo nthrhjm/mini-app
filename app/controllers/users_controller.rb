cebea362087314059e504166ff569b749cb2a19f
Hajime Nutahara <hajime.nutahara@gmail.com>
2019-08-21 21:29:35 +0900

    add my page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
-    @posts = current_user.posts.order("created_at DESC")
+    @posts = current_user.posts.page(params[:page]).per(8).order("created_at DESC")
     # @posts = Post.where(user_id: current_user.id).order("created_at DESC")
   end
 
