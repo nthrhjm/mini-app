496ea52610d2bb59144000b9e8c72d9a24c3d815
Hajime Nutahara <hajime.nutahara@gmail.com>
2019-08-24 15:59:28 +0900

    change UI color and set css to button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
     @posts = current_user.posts.page(params[:page]).per(8).order("created_at DESC")
-    # @posts = Post.where(user_id: current_user.id).order("created_at DESC")
   end
 
 end
